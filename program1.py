e9dc2b5d02ac7c1db511e3de6a519e9f584b5dfa
Rafik89PL <rafal.klimczak.1989@gmail.com>
2020-02-12 15:01:01 +0100

    server connection module add

--- program1.py
+++ program1.py
@@ -7,16 +7,21 @@ import code
 def get_credentials():
     username = raw_input("Podaj usera: ").rstrip()
     passwd = getpass('Podaj haslo dla {}: '.format(username))
-    passwd = '!w4eKuvW55e7hCIw'
     return username, passwd
 
 def get_bfservers():
-    with open('serverlist.csv', 'r') as f:
-        rows = csv.DictReader(f, delimiter=',', quotechar='\n')
+    f = open('serverlist.csv', 'r')
+    rows = csv.DictReader(f, delimiter=',', quotechar='\n')
     return rows
+
 if __name__ == "__main__":
     cred = get_credentials()
-    print (cred[0], cred[1])
+    #print (cred[0], cred[1])
     bfservers = get_bfservers()
-    print (bfservers)
-    code.interact(local=dict(globals(), **locals()))
+    for elem in bfservers:
+            server_ip = elem['IP']
+            server_hostname = elem['Hostname']
+            c =  Client(server_ip, 52311, cred[0], cred[1])
+            c.login()
+            sites = c.get('/sites')
+            print('Na serwerze {} jest {}'.format(server_hostname), len(sites))
